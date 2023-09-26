8ade5b3ed0ead22f085c1bac727e7408721cdf0c
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-09-26 09:20:59 -0300

    Initial Commit

---
+++ controllers/AuthController.js
@@ -0,0 +1,9 @@
+module.exports = class AuthController {
+    static login(req, res) {
+        res.render('auth/login')
+    }
+
+    static rigister(req, res) {
+        res.render('auth/register')
+    }
+}
