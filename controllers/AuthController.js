47ca3648566fa34a976e4743eba6a801e773c74f
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-10-07 11:13:08 -0300

    feature: sessions - auto login

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -21,5 +21,41 @@ module.exports = class AuthController {
 
             return
         }
+
+        // check if user exists
+        const checkIfUserExists = await User.findOne({ where: { email: email } })
+
+        if (checkIfUserExists) {
+            req.flash('message', 'O e-mail já está em uso!')
+            res.render('auth/register')
+
+            return
+        }
+
+        // create a password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+
+        const user = {
+            name,
+            email,
+            password: hashedPassword
+        }
+
+        try {
+            const createdUser = await User.create(user)
+
+            // initialize session
+            req.session.userid = createdUser.id
+
+            req.flash('message', 'Cadastro realizado com sucesso!')
+
+            req.session.save(() => {
+                console.log(req.session)
+                res.redirect('/')
+            })
+        } catch (err) {
+            console.log(err)
+        }
     }
 }
