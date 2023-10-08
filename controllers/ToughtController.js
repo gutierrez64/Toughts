84c4ab8dc3a899c13bff00024e03e889b8ab8c55
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-10-08 01:59:08 -0300

    refactor: dashboard css

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -7,7 +7,24 @@ module.exports = class ToughtController {
     }
 
     static async dashboard(req, res) {
-        res.render('toughts/dashboard')
+        const userId = req.session.userid
+
+        const user = await User.findOne({
+            where: {
+                id: userId,
+            },
+            include: Tought,
+            plain: true,
+        })
+
+        // check if user exists
+        if (!user) {
+            res.redirect('/login')
+        }
+
+        const toughts = user.Toughts.map((result) => result.dataValues)
+
+        res.render('toughts/dashboard', { toughts })
     }
 
     static createTought(req, res) {
@@ -26,7 +43,7 @@ module.exports = class ToughtController {
             req.flash('message', 'Pensamento criado com sucesso!')
 
             req.session.save(() => {
-                res.redirect('/tought/dashboard')
+                res.redirect('/toughts/dashboard')
             })
         } catch (error) {
             console.log('Aconteceu um erro: ' + error)
