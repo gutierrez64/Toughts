492e2da39cf88a7fdcee15e622a4e75ccbd9a839
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-10-08 02:12:04 -0300

    feature: removing toughts

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -49,4 +49,21 @@ module.exports = class ToughtController {
             console.log('Aconteceu um erro: ' + error)
         }
     }
+
+    static async removeTought(req, res) {
+        const id = req.body.id
+        const UserId = req.session.userid
+
+        try {
+            await Tought.destroy({ where: { id: id, UserId: UserId } })
+
+            req.flash('message', 'Pensamento removido com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log('Aconteceu um erro: ' + error)
+        }
+    }
 }
